d317e8f318392409056bebd9296939a82a5bb096
Michael Fedell <michaelfedell14@gmail.com>
2018-11-14 16:05:17 -0600

    Add readme with instructions and update scripts
    loader.py will only create the database with single, wide-format
    table. The user must then execute the create_populate_musicstore.sql
    file to normalize data. Instructions clearly laid out in readme

--- loader.py
+++ loader.py
@@ -2,4 +2,10 @@ import pandas
 import sqlite3
 import os
 
-print()
+if os.path.exists('./MusicStore.db'):
+    os.remove('./MusicStore.db')
+
+con = sqlite3.connect('./MusicStore.db')
+
+data = pandas.read_csv('./hw5_original.csv')
+data.to_sql('hw5_original', con, if_exists='replace')
